d71e6ea9c9236dcc7f0b9924efc5907fca7e05c6
heesae <h2s0_@naver.com>
2026-03-30 11:19:07 +0900

    이미지 파일 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as capImg } from './cap.png';
+export { default as drinkImg } from './drink.png';
+export { default as gummyImg } from './gummy.png';
+export { default as gelImg } from './gel.png';
+export { default as vitamincImg } from './vitaminc.png';
